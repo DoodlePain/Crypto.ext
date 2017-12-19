08c678c9823506165f729362bc384b30383aab70
Manuel Scarapazzi <manuelfrulloscarapazzi@gmail.com>
2017-12-19 19:39:40 +0100

    firebase.js add

---
+++ src/components/utilities/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+  
+  // Initialize Firebase
+  var database = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "425443290088"
+  };
+  firebase.initializeApp(database);
+
+
+
+// module.exports = database; 
