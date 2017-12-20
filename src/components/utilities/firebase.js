8c2e1d377cacab19e25048f689b30967381cd554
Manuel Scarapazzi <manuelfrulloscarapazzi@gmail.com>
2017-12-20 16:17:27 +0100

    firebase fix

--- src/components/utilities/firebase.js
+++ src/components/utilities/firebase.js
@@ -1,11 +1,11 @@
 import * as firebase from 'firebase';
 
 var database = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId
+  apiKey: "AIzaSyD7pjeu2NIZ51pHytDu2gZ5VFCIpThP8vg",
+  authDomain: "ccoin-24869.firebaseapp.com",
+  databaseURL: "https://ccoin-24869.firebaseio.com",
+  projectId: "ccoin-24869",
+  storageBucket: "ccoin-24869.appspot.com",
+  messagingSenderId: "425443290088"
 };
 firebase.initializeApp(database);
