cad45f69b7778a9723cdec71e30597d0e4b8aa4d
Manuel Scarapazzi <manuelfrulloscarapazzi@gmail.com>
2018-01-08 15:18:38 +0100

    Firebase update

--- src/components/utilities/firebase.js
+++ src/components/utilities/firebase.js
@@ -1,20 +1,14 @@
 import * as firebase from 'firebase';
 
+// Initialize Firebase
 var database = {
-<<<<<<< HEAD
-  apiKey: <Your api key>,
-  authDomain: <Your authDomain>,
-  databaseURL: <Your databaseURL>,
-  projectId: <Your projectId>,
-  storageBucket: <Your storageBucket>,
-  messagingSenderId: <Your messagingSenderId>
-=======
-    apiKey: "AIzaSyBixWIPRNq8BrN-zPzat4jU7urA2oKu1ok",
-    authDomain: "public-test-db.firebaseapp.com",
-    databaseURL: "https://public-test-db.firebaseio.com",
-    projectId: "public-test-db",
-    storageBucket: "",
-    messagingSenderId: "417106252205"
->>>>>>> 668677b84fb4c130cc87b6e4de00d1758c470ab5
+  apiKey: "AIzaSyD7pjeu2NIZ51pHytDu2gZ5VFCIpThP8vg",
+  authDomain: "ccoin-24869.firebaseapp.com",
+  databaseURL: "https://ccoin-24869.firebaseio.com",
+  projectId: "ccoin-24869",
+  storageBucket: "ccoin-24869.appspot.com",
+  messagingSenderId: "425443290088"
 };
 firebase.initializeApp(database);
+
+// module.exports = database;
